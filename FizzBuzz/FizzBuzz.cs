7e8ea1345c491c35a77e1c1ee48bdf53084ceaaa
Jason Deering <jason.deering@jwaala.com>
2015-07-27 15:17:17 -0500

    Fixing return value on 15

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -23,7 +23,7 @@ namespace FizzBuzz
 
         public static string Of(int i)
         {
-            if (i % 15 == 0) return "Fizz";
+            if (i % 15 == 0) return "FizzBuzz";
             if (i % 3 == 0) return "Fizz";
             if (i % 5 == 0) return "Buzz";
             return i.ToString();
