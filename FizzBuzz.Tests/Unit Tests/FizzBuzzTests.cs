ca947f1095c5459036f9acd89d8ff9ab18937d02
Jason Deering <jason.deering@jwaala.com>
2015-07-27 15:20:28 -0500

    Adding test for FizzBuzz to 20 as final verification

--- FizzBuzz.Tests/Unit Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/Unit Tests/FizzBuzzTests.cs
@@ -77,5 +77,13 @@ namespace FizzBuzz.Tests.Unit_Tests
             var actual = FizzBuzz.Of(15);
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void FizzBuzz_ToTwenty_ListIsCorrect()
+        {
+            var expected = new List<string> { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz" };
+            var actual = FizzBuzz.To(20);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
