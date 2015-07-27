e93ac57bba85c7cd86b8f90e690192f3165e543d
Jason Deering <jason.deering@jwaala.com>
2015-07-27 14:53:08 -0500

    First test

---
+++ FizzBuzz.Tests/Unit Tests/FizzBuzzTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests.Unit_Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FizzBuzz_ToOne_ContainsTheNumberOne()
+        {
+            var expected = new List<string> { "1" };
+            var actual = FizzBuzz.To(1);
+            Assert.AreSame(expected, actual);
+        }
+    }
+}
